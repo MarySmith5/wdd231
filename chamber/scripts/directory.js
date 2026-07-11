d229cadf2010fd1dd2176be80dff6f821423edcb
MarySmith5 <mary.sunshine4@gmail.com>
2026-07-11 12:37:05 -0700

    smaller member images

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,23 +1,34 @@
 const cards = document.querySelector('.cards');
+const gridbutton = document.querySelector("#grid-view");
+const listbutton = document.querySelector("#list-view");
 
-async function getMemberData() {
-    const response = await fetch('./data/members.json');
+async function getMemberDataGrid() {
+    const response = await fetch('https://marysmith5.github.io/wdd231/chamber/data/members.json');
     const data = await response.json();
-    console.table(data.displayMembers);
-    displayMembers(data.displayMembers);
+    displayMembersGrid(data.members);
 }
 
+async function getMemberDataList() {
+    const response = await fetch('https://marysmith5.github.io/wdd231/chamber/data/members.json');
+    const data = await response.json();
+    displayMembersList(data.members);
+}
 
-
-const displayMembers = (members) => {
+const displayMembersGrid = (members) => {
+    cards.replaceChildren();
+    cards.removeAttribute('id');
+    cards.setAttribute('id', 'grid');
     members.forEach((member) => {
-        console.log(member);
         let card = document.createElement('section');
         let businessName = document.createElement('h2');
         let picture = document.createElement('img');
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let url = document.createElement('a');
+        let lowerDiv = document.createElement('div');
+        let infoDiv = document.createElement('div');
+        infoDiv.setAttribute('class', 'infoDiv');
+        lowerDiv.setAttribute('class', 'lowerDiv');
 
         picture.setAttribute('src', member.image);
         picture.setAttribute('alt', `Image of  ${member.name}`);
@@ -26,20 +37,60 @@ const displayMembers = (members) => {
         picture.setAttribute('height', 'auto');
 
         url.setAttribute('href', member.url);
+        url.setAttribute('target', '_blank');
 
         businessName.textContent = `${member.name}`;
-        address.textContent = `Address: ${member.address}`;
-        phone.textContent = `Phone: ${member.phone}`;
-        url.innerHTML = "Website"
+        address.innerHTML = `<strong>Address:</strong> ${member.address}`;
+        phone.innerHTML = `<strong>Phone:</strong> ${member.phone}`;
+        url.textContent = "Website"
+
 
         card.appendChild(businessName);
-        card.appendChild(address);
-        card.appendChild(phone);
-        card.appendChild(picture);
+        infoDiv.appendChild(address);
+        infoDiv.appendChild(phone);
+        infoDiv.appendChild(url);
+        lowerDiv.appendChild(picture);
+        lowerDiv.appendChild(infoDiv);
+        card.appendChild(lowerDiv);
 
         cards.appendChild(card);
     })
 }
 
+gridbutton.addEventListener("click", () => {
+    getMemberDataGrid();
+});
+listbutton.addEventListener("click", () => {
+    getMemberDataList();
+});
 
-getMemberData(); 
+getMemberDataGrid(); 
+
+const displayMembersList = (members) => {
+    cards.replaceChildren();
+    cards.removeAttribute('id');
+    cards.setAttribute('id', 'list');
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        card.setAttribute('class', 'list')
+        let businessName = document.createElement('p');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let url = document.createElement('a');
+
+        url.setAttribute('href', member.url);
+        url.setAttribute('target', '_blank');
+
+        businessName.textContent = `${member.name}`;
+        address.textContent =  `${member.address}`;
+        phone.textContent = `${member.phone}`;
+        url.textContent = "Website"
+
+        card.appendChild(businessName);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        cards.appendChild(card);
+    })
+}
