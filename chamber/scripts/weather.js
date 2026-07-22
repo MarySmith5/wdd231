4a591cf27d5d156af1f92ef3070233b209165342
MarySmith5 <mary.sunshine4@gmail.com>
2026-07-22 09:11:38 -0700

    adding new key

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,15 +1,23 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=38.44839018545127&lon=-105.23220502161476&units=imperial&appid=c7f176d8a6326bbe90c305c9b6574067';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=38.44839018545127&lon=-105.23220502161476&units=imperial&appid=67a41587c37908bf3abddffc574a3967';
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=38.44839018545127&lon=-105.23220502161476&cnt=3&units=imperial&appid=67a41587c37908bf3abddffc574a3967';
 
-async function weatherApiFetch(url) {
+async function weatherApiFetch(url, forecastUrl) {
     try {
         const response = await fetch(url);
+        const forecastResponse = await fetch(forecastUrl);
         if (response.ok) {
-            const data = await response.json()
-            console.log(data);
-            displayResults(data);
+            const data = await response.json();
+            if (forecastResponse.ok) {
+                const forecastData = await forecastResponse.json();
+                console.log(forecastData);
+                displayResults(data, forecastData);
+            } else {
+                // throw Error(await forecastResponse.text());
+            }
+            displayResults(data, null);
         } else {
             throw Error(await response.text());
         }
@@ -18,9 +26,9 @@ async function weatherApiFetch(url) {
     }
 } 
 
-weatherApiFetch(url);
+weatherApiFetch(url, forecastUrl);
 
-function displayResults(data) {
+function displayResults(data, forecastData) {
     currentTemp.innerHTML = `<strong>${data.main.temp}&deg;F</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
